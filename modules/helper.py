2bb65fd4c0de83ef50ee04b60b457dfe51eeadd3
Pablo Ruiz <pablo.ruiz@gmail.com>
2015-05-05 14:33:50 +0200

    Initial commit.

---
+++ modules/helper.py
@@ -0,0 +1,22 @@
+'''
+Local helper methods
+'''
+
+import re
+import hashlib
+
+def __virtual__():
+    return 'helper'
+
+def regex_replace(s, find, replace):
+    """A non-optimal implementation of a regex filter"""
+    return re.sub(find, replace, s)
+
+def regex_match(s, pattern):
+    """A non-optimal implementation of a regex match"""
+    return re.match(pattern, s)
+
+def md5hash(s):
+    m = hashlib.md5()
+    m.update(s)
+    return m.hexdigest()
